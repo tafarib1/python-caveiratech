ab29dfec5d15195086389886e1d71c4b9503fae2
tafari buranelli <tafariburanelli@gmail.com>
2021-08-29 17:31:53 -0300

    conclusão aula1

---
+++ aula1.py
@@ -0,0 +1,13 @@
+nome = input("coloque seu nome: ")
+endereço1 = input('qual seu endereço: ')
+idade = input('qual sua idade? ')
+cpf = input('seu cpf: ')
+altura = input('altura: ')
+telefone = input('telefone: ')
+
+print(' '+nome,'\n',
+    'endereço:', endereço1,'\n',
+    'tem', idade, 'anos','\n',
+    'mede:', altura,'\n',
+    'cpf:', cpf,'\n',
+    'telefone:', telefone)
